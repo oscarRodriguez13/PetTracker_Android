95bdf78721a43be35f29ecfcdc4a47fbcb0f44ca
oscar <oscarrodrigomez22@gmail.com>
2024-03-06 17:50:58 -0500

    si

--- app/src/main/java/com/example/pettracker/Datos.kt
+++ app/src/main/java/com/example/pettracker/Datos.kt
@@ -4,5 +4,6 @@ class Datos {
     companion object {
         val  MY_PERMISSION_REQUEST_CAMERA = 0
         val REQUEST_IMAGE_CAPTURE = 1
+        val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     }
 }
--- app/src/main/java/com/example/pettracker/HomeActivity.kt
+++ app/src/main/java/com/example/pettracker/HomeActivity.kt
@@ -6,12 +6,17 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.pettracker.Datos.Companion.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
@@ -24,23 +29,11 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // Inicializar FusedLocationProviderClient
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        // Comprobar permisos de ubicación
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // Solicitar permisos de ubicación si no están otorgados
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
-            )
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
         } else {
-            // Si los permisos están otorgados, obtener la ubicación actual
             obtenerUbicacionActual()
         }
 
@@ -55,35 +48,16 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun obtenerUbicacionActual() {
-        // Obtener la última ubicación conocida del dispositivo
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return
         }
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
                 location?.let {
-                    // Obtener latitud y longitud
                     val latitude = it.latitude
                     val longitude = it.longitude
+                    val url = "https://www.google.com/maps/@$latitude,$longitude,15z"
 
-                    // Construir la URL de Google Maps
-                    val url = "https://www.google.com/maps/@$latitude,$longitude+z"
-
-                    // Inicializar el WebView
                     val webView = findViewById<WebView>(R.id.webViewMap)
                     webView.settings.javaScriptEnabled = true
                     webView.webViewClient = WebViewClient()
@@ -92,7 +66,13 @@ class HomeActivity : AppCompatActivity() {
             }
     }
 
-    companion object {
-        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                obtenerUbicacionActual()
+            }
+        }
     }
+
 }
