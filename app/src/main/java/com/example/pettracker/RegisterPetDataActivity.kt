fc6b37665437aa90bd24d78d3c70f3da4019041b
oscar <oscarrodrigomez22@gmail.com>
2024-03-06 22:13:04 -0500

    correcciones permisos

--- app/src/main/java/com/example/pettracker/RegisterPetDataActivity.kt
+++ app/src/main/java/com/example/pettracker/RegisterPetDataActivity.kt
@@ -1,23 +1,24 @@
 package com.example.pettracker
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.pettracker.domain.Datos.Companion.MY_PERMISSION_REQUEST_CAMERA
 import org.json.JSONArray
 import org.json.JSONObject
-import java.io.*
-
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class RegisterPetDataActivity : AppCompatActivity() {
 
@@ -27,11 +28,31 @@ class RegisterPetDataActivity : AppCompatActivity() {
     private lateinit var etAge: EditText
     private lateinit var etDescription: EditText
     private lateinit var buttonNext: Button
+    private lateinit var takePictureButton: ImageButton
 
     private var currentPetIndex = 1
     private var totalPets = 0
     private val petsList = mutableListOf<JSONObject>()
 
+    private val requestCameraPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                openCamera()
+            } else {
+                showToast("Funcionalidad limitada: permiso denegado")
+            }
+        }
+
+    private val takePictureLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                // Aquí puedes manejar la imagen capturada
+            } else {
+                showToast("Error al capturar la imagen")
+            }
+        }
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_pet_data)
@@ -44,7 +65,7 @@ class RegisterPetDataActivity : AppCompatActivity() {
         etDescription = findViewById(R.id.etDescription)
         val tvPetProgress = findViewById<TextView>(R.id.tvCurrentPet)
         buttonNext = findViewById(R.id.buttonNext)
-
+        takePictureButton = findViewById(R.id.addImageButton)
 
         // Obtener el número total de mascotas del Intent
         totalPets = intent.extras?.getString("numberPets")?.toInt() ?: 0
@@ -68,62 +89,23 @@ class RegisterPetDataActivity : AppCompatActivity() {
             }
         }
 
-        when {
-            ContextCompat.checkSelfPermission(
-                this, android.Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                // You can use the API that requires the permission.
-                Toast.makeText(this, "Gracias!", Toast.LENGTH_SHORT).show()
-                val takePictureButton = findViewById<ImageButton>(R.id.addImageButton)
-                takePictureButton.setOnClickListener(View.OnClickListener {
-                    val camerai = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    startActivityForResult(camerai, 123)
-                })
-
-            }
-            ActivityCompat.shouldShowRequestPermissionRationale(
-                this, android.Manifest.permission.CAMERA) -> {
-                requestPermissions(
-                    arrayOf(android.Manifest.permission.CAMERA),
-                    MY_PERMISSION_REQUEST_CAMERA)
-            }
-            else -> {
-                // You can directly ask for the permission.
-                requestPermissions(
-                    arrayOf(android.Manifest.permission.CAMERA),
-                    MY_PERMISSION_REQUEST_CAMERA)
+        // Configurar el botón de tomar foto
+        takePictureButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                openCamera()
+            } else {
+                requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
             }
         }
-
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        //val textV = findViewById<TextView>(R.id.textView3)
-        when (requestCode) {
-            MY_PERMISSION_REQUEST_CAMERA -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // Permission is granted. Continue the action or workflow
-                    // in your app.
-                    Toast.makeText(this, "Gracias!", Toast.LENGTH_SHORT).show()
-                    val takePictureButton = findViewById<ImageButton>(R.id.addImageButton)
-                    takePictureButton.setOnClickListener(View.OnClickListener {
-                        val camerai = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                        startActivityForResult(camerai, 123)
-                    })
-                } else {
-                    // Explain to the user that the feature is unavailable
-                    //textV.text = "PERMISO DENEGADO"
-                    //textV.setTextColor(Color.RED)
-                    Toast.makeText(this, "Funcionalidades limitadas!", Toast.LENGTH_SHORT).show()
-                }
-                return
-            }
-            else -> {
-                // Ignore all other requests.
-            }
-        }
+    private fun openCamera() {
+        val camerai = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        takePictureLauncher.launch(camerai)
     }
 
     private fun validatePetData(): Boolean {
@@ -211,5 +193,4 @@ class RegisterPetDataActivity : AppCompatActivity() {
         // Muestra un Toast con el mensaje proporcionado
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
-
 }
