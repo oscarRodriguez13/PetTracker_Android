e980435ae62f3c431106e18398695892305ae3dd
estudiante <plays867yer@gmail.com>
2024-03-07 08:32:32 -0500

    Strings

--- app/src/main/java/com/example/pettracker/domain/Datos.kt
+++ app/src/main/java/com/example/pettracker/domain/Datos.kt
@@ -6,6 +6,6 @@ class Datos {
         val REQUEST_IMAGE_CAPTURE = 1
         val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
         val WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 123
-        //hola
+        //hola jaja
     }
 }
