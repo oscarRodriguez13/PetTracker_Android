fe0727d534b1b1c09ce1d535e0181c41d8f95dd9
estudiante <plays867yer@gmail.com>
2024-03-06 19:26:42 -0500

    Actividad buscar mascota (boton)

--- app/src/main/java/com/example/pettracker/PaginaPaseoActivity.kt
+++ app/src/main/java/com/example/pettracker/PaginaPaseoActivity.kt
@@ -1,11 +1,22 @@
 package com.example.pettracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PaginaPaseoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagina_paseo)
+
+        val mascotaButton = findViewById<Button>(R.id.ir_mascota)
+        mascotaButton.setOnClickListener {
+            val intent = Intent(
+                applicationContext,
+                BuscarMascotaActivity::class.java
+            )
+            startActivity(intent)
+        }
     }
 }
