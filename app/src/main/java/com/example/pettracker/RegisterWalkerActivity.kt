729a583e017b4175dee3d61e2be8aba4f31b8113
oscar <oscarrodrigomez22@gmail.com>
2024-03-07 07:11:24 -0500

    correcciones permisos

--- app/src/main/java/com/example/pettracker/RegisterWalkerActivity.kt
+++ app/src/main/java/com/example/pettracker/RegisterWalkerActivity.kt
@@ -1,22 +1,42 @@
 package com.example.pettracker
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class RegisterWalkerActivity : AppCompatActivity() {
+
+    private lateinit var etName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etExperience: EditText
+    private lateinit var buttonRegister: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_walker)
 
-        val registerButton = findViewById<Button>(R.id.button)
-        registerButton.setOnClickListener {
-            val intent = Intent(
-                applicationContext,
-                LoginActivity::class.java
-            )
+        etName = findViewById(R.id.etName)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        etExperience = findViewById(R.id.etExperience)
+        buttonRegister = findViewById(R.id.button)
+
+        buttonRegister.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
+
 }
