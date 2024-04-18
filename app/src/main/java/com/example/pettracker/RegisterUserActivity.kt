a0078dd687281e0d60189e3dfbef33c2e58a67e1
oscar <oscarrodrigomez22@gmail.com>
2024-04-18 07:55:39 -0500

    pickear foto y tomar de la camara

--- app/src/main/java/com/example/pettracker/RegisterUserActivity.kt
+++ app/src/main/java/com/example/pettracker/RegisterUserActivity.kt
@@ -1,13 +1,29 @@
 package com.example.pettracker
 
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.pettracker.domain.Datos
 
 class RegisterUserActivity : AppCompatActivity() {
+
+    private lateinit var fotoPaseador: ImageView
+    private lateinit var imagePickerLauncher: ActivityResultLauncher<String>
+    private lateinit var takePictureLauncher: ActivityResultLauncher<Uri>
+    private var photoURI: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -41,6 +57,85 @@ class RegisterUserActivity : AppCompatActivity() {
             }
         }
 
+        fotoPaseador = findViewById(R.id.icn_paseador)
+
+        // Preparar el lanzador para el resultado de selección de imagen.
+        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            uri?.let {
+                fotoPaseador.setImageURI(uri)
+            }
+        }
+
+        // Preparar el lanzador para el resultado de tomar foto.
+        takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success) {
+                photoURI?.let {
+                    fotoPaseador.setImageURI(it)
+                }
+            }
+        }
+
+        findViewById<ImageButton>(R.id.agregarFotoView).setOnClickListener {
+            // Intent explícito para seleccionar una imagen de la galería
+            imagePickerLauncher.launch("image/*")
+        }
+
+        findViewById<ImageButton>(R.id.tomarFotoView).setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this, android.Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //Lanzamos la camara
+                    openCamera()
+                }
+
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, android.Manifest.permission.CAMERA
+                ) -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.CAMERA),
+                        Datos.MY_PERMISSION_REQUEST_CAMERA
+                    )
+                }
+
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.CAMERA),
+                        Datos.MY_PERMISSION_REQUEST_CAMERA
+                    )
+                }
+            }
+        }
+
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Foto nueva")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Tomada desde la aplicacion del Taller 2")
+        photoURI = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        photoURI?.let { uri ->
+            takePictureLauncher.launch(uri)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            Datos.MY_PERMISSION_REQUEST_CAMERA -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    openCamera()
+                } else {
+                    Toast.makeText(this, "Funcionalidades limitadas!", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 
     private fun verificarCamposLlenos(vararg campos: EditText): Boolean =
--- app/src/main/java/com/example/pettracker/RegisterWalkerActivity.kt
+++ app/src/main/java/com/example/pettracker/RegisterWalkerActivity.kt
@@ -1,16 +1,24 @@
 package com.example.pettracker
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.pettracker.domain.Datos
 import de.hdodenhof.circleimageview.CircleImageView
 import org.json.JSONArray
 import org.json.JSONObject
@@ -26,6 +34,9 @@ class RegisterWalkerActivity : AppCompatActivity() {
     private lateinit var etPassword: EditText
     private lateinit var etExperience: EditText
     private lateinit var buttonRegister: Button
+    private lateinit var imagePickerLauncher: ActivityResultLauncher<String>
+    private lateinit var takePictureLauncher: ActivityResultLauncher<Uri>
+    private var photoURI: Uri? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,8 +57,53 @@ class RegisterWalkerActivity : AppCompatActivity() {
         }
 
         val perfilView = findViewById<CircleImageView>(R.id.icn_paseador)
-        perfilView.setOnClickListener {
 
+        // Preparar el lanzador para el resultado de selección de imagen.
+        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            uri?.let {
+                perfilView.setImageURI(uri)
+            }
+        }
+
+        // Preparar el lanzador para el resultado de tomar foto.
+        takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success) {
+                photoURI?.let {
+                    perfilView.setImageURI(it)
+                }
+            }
+        }
+
+        findViewById<ImageButton>(R.id.agregarFotoView).setOnClickListener {
+            // Intent explícito para seleccionar una imagen de la galería
+            imagePickerLauncher.launch("image/*")
+        }
+
+        findViewById<ImageButton>(R.id.tomarFotoView).setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this, android.Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //Lanzamos la camara
+                    openCamera()
+                }
+
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, android.Manifest.permission.CAMERA
+                ) -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.CAMERA),
+                        Datos.MY_PERMISSION_REQUEST_CAMERA
+                    )
+                }
+
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.CAMERA),
+                        Datos.MY_PERMISSION_REQUEST_CAMERA
+                    )
+                }
+            }
         }
     }
 
@@ -94,6 +150,36 @@ class RegisterWalkerActivity : AppCompatActivity() {
         }
 
         return jsonArray
+    }
+
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Foto nueva")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Tomada desde la aplicacion del Taller 2")
+        photoURI = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        photoURI?.let { uri ->
+            takePictureLauncher.launch(uri)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            Datos.MY_PERMISSION_REQUEST_CAMERA -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    openCamera()
+                } else {
+                    Toast.makeText(this, "Funcionalidades limitadas!", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 
 }
